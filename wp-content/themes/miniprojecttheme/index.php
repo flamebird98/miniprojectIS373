e935b2a582e17d67b1c529c9a412a21e49154292
Sade Eversley <sadeeversley@gmail.com>
2019-03-27 08:32:01 -0400

    Commit of editing files

--- wp-content/themes/miniprojecttheme/index.php
+++ wp-content/themes/miniprojecttheme/index.php
@@ -1,7 +1,30 @@
-<?php
+<?php ?>
 /**
  * Created by PhpStorm.
  * User: sadeeversley
  * Date: 2019-03-26
  * Time: 19:00
  */
+<!DOCTYPE html>
+	<head>
+		<meta charset="UTF-8">
+		<title>How To Convert HTML Template to WordPress Theme - WPExplorer</title>
+		<link rel="stylesheet" type="text/css" media="all" href="style.css"/>
+	</head>
+	<body>
+		<div id="wrap">
+			<header class="header">
+				<p>This is header section. Put your logo and other details here.</p>
+			</header><!-- .header -->
+			<div class="content">
+				<p>This is the main content area.</p>
+			</div><!-- .content -->
+			<div class="sidebar">
+				<p>This is the side bar</p>
+			</div><!-- .sidebar -->
+			<footer class="footer">
+				<p>And this is the footer.</p>
+			</footer><!-- .footer -->
+		</div><!-- #wrap -->
+	</body>
+</html>
