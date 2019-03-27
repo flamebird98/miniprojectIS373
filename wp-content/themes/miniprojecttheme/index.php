52582051041e4df01d444104a7c8d93791a41a75
Sade Eversley <sadeeversley@gmail.com>
2019-03-27 08:43:00 -0400

    Commit of adding files

--- wp-content/themes/miniprojecttheme/index.php
+++ wp-content/themes/miniprojecttheme/index.php
@@ -37,7 +37,6 @@
 	</div><!--.content-->
 
 
-
 <?php get_header(); ?>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
---
+++ wp-content/themes/miniprojecttheme/sidebar.php
@@ -0,0 +1,9 @@
+<?php ?>
+/**
+ * Created by PhpStorm.
+ * User: sadeeversley
+ * Date: 2019-03-27
+ * Time: 08:42
+ */
+
+<?php dynamic_sidebar( 'sidebar' ); ?>
